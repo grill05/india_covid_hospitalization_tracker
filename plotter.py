f84d66ac84fc92ec7a1f646cca04b0d680f968e4
Your Name <you@example.com>
2022-01-10 06:57:45 +0530

    auto

--- plotter.py
+++ plotter.py
@@ -303,7 +303,7 @@ if __name__=='__main__':
   a.close()
   
   #kl,tg,ap,uk
-  for state in ['kerala','telangana','ap','uttarakhand','chandigarh','nagpur','nashik','vadodara','gandhinagar','wb','pb','jammu','goa','rajasthan','bihar']:
+  for state in ['kerala','telangana','ap','uttarakhand','chandigarh','nagpur','nashik','vadodara','gandhinagar','wb','pb','jammu','goa','rajasthan','bihar','ludhiana']:
     print(state.upper())
     a=open(state+'.html','w')  
     
@@ -315,6 +315,8 @@ if __name__=='__main__':
       d,c=zip(*dp.get_cases_district('gj',state.capitalize()))
     elif state in ['jammu']:
       d,c=zip(*dp.get_cases_district('jk',state.capitalize()))
+    elif state in ['ludhiana']:
+      d,c=zip(*dp.get_cases_district('pb',state.capitalize()))
     else:
       fixed_state_name=state.capitalize().replace('Ap','ap').replace('Wb','wb').replace('Pb','pb')
       d,c=zip(*dp.get_cases(fixed_state_name,delta=True))
@@ -327,12 +329,13 @@ if __name__=='__main__':
     fig = make_subplots(specs=[[{"secondary_y": True}]])
     
     fig.add_trace(go.Scatter(x=x2['date'],y=x2['cases'], name="Daily cases",mode='lines+markers'),secondary_y=False)
-    fig.add_trace(go.Scatter(x=x2['date'],y=x2['occupied_normal_beds'], name="Occupied general Beds",mode='lines+markers'),secondary_y=True)
+    if state not in ['ludhiana']:
+      fig.add_trace(go.Scatter(x=x2['date'],y=x2['occupied_normal_beds'], name="Occupied general Beds",mode='lines+markers'),secondary_y=True)
     if state not in ['wb']:
       if state not in ['goa','bihar']:
         fig.add_trace(go.Scatter(x=x2['date'],y=x2['occupied_o2_beds'], name="Occupied O2 Beds",mode='lines+markers'),secondary_y=True)
       fig.add_trace(go.Scatter(x=x2['date'],y=x2['occupied_icu_beds'], name="Occupied ICU Beds",mode='lines+markers'),secondary_y=True)
-      if state not in ['telangana','goa','bihar']:
+      if state not in ['telangana','goa','bihar','ludhiana']:
         fig.add_trace(go.Scatter(x=x2['date'],y=x2['occupied_ventilator_beds'], name="Occupied Ventilator Beds",mode='lines+markers'),secondary_y=True)
     
     fig.update_xaxes(title_text='Date')
@@ -352,6 +355,7 @@ if __name__=='__main__':
     if state=='wb':available_columns=['occupied_normal_beds','total_normal_beds']
     elif state=='pb':available_columns=['occupied_normal_beds','occupied_o2_beds','occupied_icu_beds','occupied_ventilator_beds','total_o2_beds','total_icu_beds','total_ventilator_beds']
     elif state in ['goa','bihar']:available_columns=['occupied_normal_beds','occupied_icu_beds','total_normal_beds','total_icu_beds']
+    elif state in ['ludhiana']:available_columns=['occupied_o2_beds','occupied_icu_beds','occupied_o2_beds','total_icu_beds']
     
     fig=px.line(x2,x='date',y=available_columns,markers=True,title='Hospital bed occupancy/capacity in '+state.upper())
     a.write(fig.to_html(full_html=False, include_plotlyjs='cdn'))
